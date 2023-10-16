668b31e5f23ecd360283a2f7ae424232187825c8
zohra-neda <zohra.neda@gmail.com>
2023-10-16 22:46:54 +0430

    Migrate category and exchange tables

--- db/migrate/20231016123005_create_categories.rb
+++ db/migrate/20231016123005_create_categories.rb
@@ -1,7 +1,9 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-
+      t.references :author, foreign_key: { to_table: 'users' }
+      t.string :name
+      t.string :icon
       t.timestamps
     end
   end
