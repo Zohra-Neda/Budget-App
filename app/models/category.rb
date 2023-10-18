77a37906ecc33fe62ace6080d63091cc14b8698f
zohra-neda <zohra.neda@gmail.com>
2023-10-18 09:15:52 +0430

    Set up associations between models and use validations for models attributes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,16 @@
 class Category < ApplicationRecord
-    belongs_to :user, foreign_key: 'author_id'
-    has_many :exchanges, foreign_key: 'category_id'
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    has_many :exchanges, class_name: 'Exchange', foreign_key: 'category_id'
+
+    validates :name, presence: true, length: { maximum: 30, too_long: '%<count>s charecters is the maximum allowed' }
+     validates :icon, presence: true
+
+
+    def total_amount
+        total = 0
+        exchanges.each do |exchange|
+            total += exchange.amount.to_i
+        end
+        total
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true, length: { maximum: 30, too_long: '%<count>s charecters is the maximum allowed' }
 end
