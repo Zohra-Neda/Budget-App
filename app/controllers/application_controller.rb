dc246081adc6a3206da5e7c082ab3e4947caaf8a
zohra-neda <zohra.neda@gmail.com>
2023-10-18 15:24:35 +0430

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
 end
 
-
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password) }
-  end
+def update_allowed_parameters
+  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password) }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true, length: { maximum: 30, too_long: '%<count>s charecters is the maximum allowed' }
+  validates :name, presence: true,
+                   length: { maximum: 30, too_long: '%<count>s charecters is the maximum allowed' }
 end
