1a0cf32054e1414b08ec793cb841d6a06ca1be9e
zohra-neda <zohra.neda@gmail.com>
2023-10-19 11:06:27 +0430

    Update render.yaml file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_051807) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_19_063407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_051807) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
