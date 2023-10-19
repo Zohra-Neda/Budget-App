c1998d65611d28748a7e1729c67f4dbe08cab53d
zohra-neda <zohra.neda@gmail.com>
2023-10-19 12:11:21 +0430

    Update render.yaml file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,5 @@ class ApplicationController < ActionController::Base
 end
 
 def update_allowed_parameters
-  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password) }
+  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
 end
